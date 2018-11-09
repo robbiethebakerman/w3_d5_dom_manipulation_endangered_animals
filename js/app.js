546a209ed6648de4d051db4c1379a011c62f1355
Robbie Baker <robbiethebakerman@googlemail.com>
2018-11-09 16:45:16 +0000

    Creates html layout and basic structure for app.js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.new-animal-form');
+  form.addEventListener('submit', handleFormSubmit);
+});
+
+const handleFormSubmit = function(event) {
+
+};
+
+const createAnimalListElement = function(form) {
+
+};
+
+const deleteAll = function(event) {
+
+};
